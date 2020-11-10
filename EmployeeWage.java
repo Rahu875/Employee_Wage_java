4ee3e607aca3ed07b9cdf3d3652deb9c48ecd89b
“Enter <rahulrajan5410@gmail.com>
2020-11-10 13:48:02 +0530

    ADDED employee attendence

--- EmployeeWage.java
+++ EmployeeWage.java
@@ -1,1 +1,11 @@
-"welcome"
+
+int IS_FULL_TIME =1;
+double empCheck = Math.floor(Math.random() * 10) %2;
+if (empCheck == IS_FULL_TIME)
+	system.out.ptintln("Employee is Present");
+else
+	system.out.println("Employee is Absent");
+
+
+
+
